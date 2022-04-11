5dc64300fc78463da8f237cea446f9b10a6948d5
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-04-11 14:49:32 -0700

    installed and added express

--- api/server.js
+++ api/server.js
@@ -1,3 +1,6 @@
-// BUILD YOUR SERVER HERE
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require('express-session');
 
 module.exports = {}; // EXPORT YOUR SERVER instead of {}
